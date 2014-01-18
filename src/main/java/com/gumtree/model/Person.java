ed81ec984a6976ee250b29872ddc4f5c0ea7e25d
Diego Salvado <dsalvado@hotmail.es>
2014-01-18 16:48:20 +0100

    Commit no.5: Completed model class Person

--- src/main/java/com/gumtree/model/Person.java
+++ src/main/java/com/gumtree/model/Person.java
@@ -1,5 +1,38 @@
 package com.gumtree.model;
 
+import java.util.Date;
+
 public class Person {
 
+	private String name;
+	private String gender;
+	private Date age;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public Date getAge() {
+		return age;
+	}
+	
+	public void setAge(Date age) {
+		this.age = age;
+	}
+	
+	
+	
 }
